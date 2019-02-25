a63cad90553216b991f706c0ea05372f68a35719
Matt Barry <matt@hazelmollusk.org>
2019-02-25 16:30:31 -0500

    fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-/* eslint-disable no-console */
-const logger = require('./logger');
-const app = require('./app');
-const port = app.get('port');
-const server = app.listen(port);
+// eslint-disable-next-line no-unused-vars
+const services = require('./services');
 
-process.on('unhandledRejection', (reason, p) =>
-  logger.error('Unhandled Rejection at: Promise ', p, reason)
-);
-
-server.on('listening', () =>
-  logger.info('Feathers application started on http://%s:%d', app.get('host'), port)
-);
+module.exports = function (app) {
+  app.configure(services);
+};
